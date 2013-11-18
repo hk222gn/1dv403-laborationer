10013e24891941b6f734412d1a1afd76a0556131
hk222gn <hk222gn>
2013-11-18 20:52:03 +0100

    guess färdig

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,26 +2,30 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+    var secret = Math.floor(Math.random() * (100 - 1) + 1) + 1; //slumpar tal
+    var guesses = 0;
+
 	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var guess = function(number){
+    var guess = function (number) {
+        guesses += 1;
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
-
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+		
+		if (number == secret) {
+		    return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + guesses + " gissningar för att hitta det."]
+		}
+		else if (number < secret && number <= 100 && number >= 1) {
+		    return [false, "Det hemliga talet är högre!"]
+		}
+		else if (number > secret && number <= 100 && number >= 1) {
+		    return [false, "Det hemliga talet är lägre!"]
+		}
+		else {
+		    return [false, "Talet är utanför intervallet 1 - 100"]
+		}
 	};
 	
 	// ------------------------------------------------------------------------------
-
-
 
 	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
 	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
