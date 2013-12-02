0517f41333a65e01cb9f07b55cff2e1d7bf9e589
hk222gn <hk222gn>
2013-12-03 00:26:37 +0100

    skapa med, klar, skriv ut påbörjad

--- 2-labbymezzage/script/MessageBoard.js
+++ 2-labbymezzage/script/MessageBoard.js
@@ -1,19 +1,52 @@
 "use strict";
 
-
 var MessageBoard = {
-        
+
     messages: [],
 
-    init:function(e) {
-        var mess = new Message("Tester", new Date());
+    init: function (e) {
+        //var mess = new Message("Tester", new Date());
         //alert(mess);
         //alert(mess.getText());
         //mess.setText("Rrrtser");
         //alert(mess);
         MessageBoard.messages[0] = new Message("Tast", new Date());
-    }
 
+        var button = document.getElementById("button");
+        button.onclick = function (e) {
+            GetMessageFromTextfield()
+        }
+        var GetMessageFromTextfield = function () {
+            MessageBoard.messages[0] = new Message(document.getElementById("textarea").value, new Date());
+            console.log(MessageBoard.messages[0].getText());
+            console.log(MessageBoard.messages[0].getDate());
+            alert(MessageBoard.messages[0]);
+        }
+
+        var renderMessages = function () {// varfor funkar inte renderMessage: function()
+
+            document.getElementById("messagearea").innerHTML = "";
+
+            for (var i = 0; i < MessageBoard.messages.length; i+=1) {
+                MessageBoard.renderMessages(i);
+            }
+        }
+
+        var renderMessage = function(messageID) {
+            //dona
+
+        }
+    }
 }
-    console.log(MessageBoard.messages[0]);
-window.onload = MessageBoard.init();
+
+    
+
+
+
+//if (button.AddEventListener) {
+//    button.click("onclick", function () { //nytt objekt med texten. 
+//        // Anvand value pa textarea objektet for att fa ut vad anvandaren skrev in.
+//        console.log("RUMPE");
+//        button.click(function () {console.log("RUMPE");});
+//    });
+//}
