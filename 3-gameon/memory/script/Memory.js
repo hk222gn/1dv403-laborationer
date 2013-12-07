c85f15b7bf91a56340acc98de2bde3e44431377c
hk222gn <hk222gn>
2013-12-07 19:46:16 +0100

    bilder på g

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -5,13 +5,28 @@ var Memory = {
     randArray: [],
 
     init: function () {
-        Memory.randArray = RandomGenerator.getPictureArray(4, 4);
-        console.log(Memory.randArray[1]);
-        console.log(Memory.randArray[3]);
-        console.log(Memory.randArray[2]);
+        Memory.randArray = RandomGenerator.getPictureArray(4, 2);
+        Memory.RenderTable();
     },
 
-    
+    RenderTable: function () {
+        var div = document.getElementById("table");
+        var thead = document.createElement("table");
+        var img = document.createElement("img");
+        thead.setAttribute("border", 1);
+        div.appendChild(thead);
+        var tbody = document.createElement("tbody");
+        for (var i = 0; i < 2; i++) {
+            var tr = document.createElement("tr");
+            thead.appendChild(tr);
+            for (var j = 0; j < 4; j++) {
+                var td = document.createElement("td");
+                tr.appendChild(td);
+                img.setAttribute("src", "pics/0.png");
+                td.appendChild(img);
+            }
+        }
+    }
 
 
 }
