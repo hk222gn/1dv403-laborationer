e94ad8614f416239e6aa129dc85d4131736be41e
hk222gn <hk222gn>
2013-12-09 00:36:08 +0100

    En bit till

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -1,34 +1,72 @@
 "use strict";
 
+var rows = 4;
+var cells = 4;
+var counter = 0;
+
 var Memory = {
 
-    randArray: [],
+    randArr: [],
 
+    picArr: ["pics/0.png",
+                        "pics/1.png",
+                        "pics/2.png",
+                        "pics/3.png",
+                        "pics/4.png",
+                        "pics/5.png",
+                        "pics/6.png",
+                        "pics/7.png",
+                        "pics/8.png"],
     init: function () {
-        Memory.randArray = RandomGenerator.getPictureArray(4, 2);
-        Memory.RenderTable();
+        Memory.randArr = RandomGenerator.getPictureArray(rows, cells);//Ska ? bilden vara pa alla forst, sen bytas? med hjalp av ett ID?
+        Memory.renderTable();
     },
 
-    RenderTable: function () {
+    renderTable: function () {
+        //array with all pictures
+        
+
+        var picID = 0;
+
         var div = document.getElementById("table");
         var thead = document.createElement("table");
-        var img = document.createElement("img");
+        var tbody = document.createElement("tbody");
+        
         thead.setAttribute("border", 1);
         div.appendChild(thead);
-        var tbody = document.createElement("tbody");
-        for (var i = 0; i < 2; i++) {
+        
+        //Creates the table and adds the images( + links)
+        for (var i = 0; i < rows; i++) {
             var tr = document.createElement("tr");
             thead.appendChild(tr);
-            for (var j = 0; j < 4; j++) {
+
+            for (var j = 0; j < cells; j++) {
+
+                picID += 1;
+
                 var td = document.createElement("td");
+                var img = document.createElement("img");
+                var a = document.createElement("a");
+                a.href = "#";
+                img.src = Memory.picArr[0];//make dynamic
+                img.id = picID;//unsure
+                //img.src = picArr[Memory.randArr[picID]];
                 tr.appendChild(td);
-                img.setAttribute("src", "pics/0.png");
-                td.appendChild(img);
+                td.appendChild(a);
+                a.appendChild(img);
+
             }
         }
+        a.onclick = Memory.flipPicture;
+    },
+    flipPicture: function () {
+        counter += 1;
+        document.getElementById("1").src = Memory.picArr[Memory.randArr[1]];//unsure
+        console.log("as");
+        //Document?
+        //gor sa bilden byts
+        //on click, find out about the image number? in randArr?
     }
-
-
 }
 
 window.onload = Memory.init;
