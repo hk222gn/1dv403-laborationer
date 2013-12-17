d77c54a62f3d0abbe23d43c7046fec047c02a760
hk222gn <hk222gn>
2013-12-18 00:57:09 +0100

    Mer donat v2

--- 4-forms/script/validate.js
+++ 4-forms/script/validate.js
@@ -2,10 +2,14 @@
 
 var Validate = {
     arr: [],
+    required: 4,
 
     init: function () {
 
         var form = document.querySelector("#form");
+        var footer = document.createElement("footer");
+        footer.setAttribute("class", "invalid");
+        var text = document.createTextNode("None of the red marked fields can be empty! Please fill them in!");
 
         Validate.getElements(form);
             
@@ -17,12 +21,33 @@ var Validate = {
         Validate.checkMail(Validate.arr[3]);
 
         form.onsubmit = function () {
+            var length = 0;
 
-            //If there are a ton of these, use a for loop to get the amount of "valid" classes, then compare to the amount there should be.
-            if (Validate.arr[0].className && Validate.arr[1].className && Validate.arr[2].className && Validate.arr[3].className == "valid")
+            for (var i = 0; i < Validate.required; i++) {
+                if (Validate.arr[i].className == "valid") {
+                    length += 1;
+                }
+            }
+            if (length === Validate.required) {
+                form.removeChild(footer);
                 return true;
-            else
+            }
+            else {
+                footer.appendChild(text);
+                form.appendChild(footer);
                 return false;
+            }
+            //If there are not a ton of these, this is a possibility.
+
+            //if (Validate.arr[0].className && Validate.arr[1].className && Validate.arr[2].className && Validate.arr[3].className == "valid") {
+            //    form.removeChild(footer);
+            //    return true;
+            //}
+            //else {
+            //    footer.appendChild(text);
+            //    form.appendChild(footer);
+            //    return false;
+            //}
         }
     },
 
@@ -36,8 +61,9 @@ var Validate = {
     isEmpty: function (el) {
 
         el.onblur = function () {
-            if (el.value == '')
+            if (el.value == '') {
                 el.setAttribute("class", "invalid");
+            }
             else
                 el.setAttribute("class", "valid");//nagot sadant?
         }
@@ -47,6 +73,7 @@ var Validate = {
         var re = "^\\d{5}?$";
 
         el.onblur = function () {
+            el.value = el.value.replace(" ","").replace("-", "").replace("SE","")
 
             if (el.value.match(re) !== null)
                 el.setAttribute("class", "valid");
