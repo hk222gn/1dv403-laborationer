5a7ad122d5fdeef24c2a0c8e0df82acda43b9213
hk222gn <hk222gn>
2013-12-18 20:30:29 +0100

    Klar?

--- 4-forms/script/validate.js
+++ 4-forms/script/validate.js
@@ -3,13 +3,14 @@
 var Validate = {
     arr: [],
     required: 4,
+    form1: document.querySelector("#form"),
 
     init: function () {
 
         var form = document.querySelector("#form");
         var footer = document.createElement("footer");
         footer.setAttribute("class", "invalid");
-        var text = document.createTextNode("None of the red marked fields can be empty! Please fill them in!");
+        var text = document.createTextNode("None of the red marked fields can be empty! Please fill them in correctly!");
 
         Validate.getElements(form);
             
@@ -28,26 +29,22 @@ var Validate = {
                     length += 1;
                 }
             }
+
             if (length === Validate.required) {
-                form.removeChild(footer);
-                return true;
+                if (footer.parentNode == form) {
+                    form.removeChild(footer);
+                }
+                if (Validate.popupWindow()) {
+                    
+                    return true;
+                }
+                return false;
             }
             else {
                 footer.appendChild(text);
                 form.appendChild(footer);
                 return false;
             }
-            //If there are not a ton of these, this is a possibility.
-
-            //if (Validate.arr[0].className && Validate.arr[1].className && Validate.arr[2].className && Validate.arr[3].className == "valid") {
-            //    form.removeChild(footer);
-            //    return true;
-            //}
-            //else {
-            //    footer.appendChild(text);
-            //    form.appendChild(footer);
-            //    return false;
-            //}
         }
     },
 
@@ -65,7 +62,7 @@ var Validate = {
                 el.setAttribute("class", "invalid");
             }
             else
-                el.setAttribute("class", "valid");//nagot sadant?
+                el.setAttribute("class", "valid");
         }
     },
 
@@ -91,6 +88,64 @@ var Validate = {
             else
                 el.setAttribute("class", "invalid");
         }
+    },
+
+    popupWindow: function () {
+        var temp;
+        var temp2;
+
+        var el = document.getElementById("overlay");
+        el.setAttribute("class", "visible");
+        var div = document.createElement("div");
+        div.setAttribute("id", "popup");
+
+        
+        var header = document.createElement("header");
+        var confirm = document.createElement("input");
+        var cancel = document.createElement("input");
+        confirm.setAttribute("type", "submit");
+        confirm.setAttribute("value", "Godkan kop");
+        cancel.setAttribute("type", "submit");
+        cancel.setAttribute("value", "Avbryt");
+        header.appendChild(document.createTextNode("Godkan ditt kop"));
+
+        confirm.onclick = function () { el.setAttribute("class", "hidden"); Validate.form1.submit(); }
+        cancel.onclick = function () { el.setAttribute("class", "hidden"); el.removeChild(div);};
+
+        el.appendChild(div);
+        div.appendChild(header);
+
+        for (var i = 0; i < Validate.required; i++) {
+            var d = document.createElement("div");
+            d.setAttribute("class", "name");
+            temp2 = Validate.arr[i].name;
+            d.appendChild(document.createTextNode(temp2));
+            header.appendChild(d);
+
+
+            var dd = document.createElement("div");
+            dd.setAttribute("class", "values");
+            temp = Validate.arr[i].value;
+            dd.appendChild(document.createTextNode(temp));
+            header.appendChild(dd);
+        }
+        
+        
+        div.appendChild(cancel);
+        div.appendChild(confirm);
     }
 };
 window.onload = Validate.init;
+
+
+//If there are not a ton of these, this is a possibility.
+
+//if (Validate.arr[0].className && Validate.arr[1].className && Validate.arr[2].className && Validate.arr[3].className == "valid") {
+//    form.removeChild(footer);
+//    return true;
+//}
+//else {
+//    footer.appendChild(text);
+//    form.appendChild(footer);
+//    return false;
+//}
