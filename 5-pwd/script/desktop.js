5ba58fb42fc114462482adc333ce574e57d6d78d
hk222gn <hk222gn>
2014-01-01 19:50:34 +0100

    Klar 3

--- 5-pwd/script/desktop.js
+++ 5-pwd/script/desktop.js
@@ -3,13 +3,109 @@ var desktop = {
 
     init: function () {
         var a = document.getElementById("menuicon");
+        var clickable = 0;
         
+        a.onclick = function (e) {
+            e.preventDefault();
+            if (clickable === 0) {
+                var div = document.getElementById("desktop");
+                var window = document.createElement("div");
+                window.setAttribute("id", "window");
+
+                var header = document.createElement("div");
+                header.setAttribute("id", "header");
+                var camera = document.createElement("img");
+                camera.src = "pics/camera.png";
+                var t = document.createTextNode("Image viewer");
+                var ax = document.createElement("a");
+                ax.setAttribute("id", "closeimageviewer");
+                var x = document.createElement("img");
+                x.src = "pics/x.png";
+
+                var content = document.createElement("div");
+                content.setAttribute("id", "content");
+
+                var footer = document.createElement("div");
+                footer.setAttribute("id", "footer");
+
+                div.appendChild(window);
+
+                window.appendChild(header);
+                header.appendChild(camera);
+                header.appendChild(t);
+                header.appendChild(ax);
+                ax.appendChild(x);
+
+                window.appendChild(content);
+                window.appendChild(footer);
+                
+                desktop.renderImages();
+
+                clickable += 1;
+            }
+            ax.onclick = function (e) {
+                e.preventDefault();
+                var div = document.getElementById("desktop");
+                div.removeChild(window);
+                clickable -= 1;
+            }
+        }
     },
 
+    renderImages: function () {
+        //appends the pictures correctly and then returns nothing. Also handles the loading gif.
+        var url = "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/";
+        var loader = document.createElement("img");
+        loader.src = "pics/ajax-loader.gif";
+        footer.appendChild(loader);
 
+        new AjaxCon(url, function (data) {
+            footer.removeChild(loader);
+            var imageInfo = JSON.parse(data);
 
+            var heightAndWidth = desktop.getHighestWidthAndHeight(imageInfo);
+            
+            for (var i = 0; i < imageInfo.length; i++) {
+                var a = document.createElement("a");
+                var img = document.createElement("img");
+                a.style.width = heightAndWidth[0] + 5 + "px";
+                a.style.height = heightAndWidth[1] + 5 + "px";
+                a.href = "#";
+                img.src = imageInfo[i].thumbURL;
 
+                desktop.changeBackground(imageInfo[i].URL, a);
 
+                content.appendChild(a);
+                a.appendChild(img);
+
+            }
+
+        });
+    },
+
+    changeBackground: function (info, a) {
+        a.onclick = function (e) {
+            e.preventDefault();
+
+            var d = document.getElementById("desktop");
+            d.style.backgroundImage = "url('" + info + "')";
+        }
+    },
+
+    getHighestWidthAndHeight: function (info) {
+        var width = 0;
+        var height = 0;
+
+        for (var i in info) {
+            if (info[i].thumbWidth > width) {
+                width = info[i].thumbWidth;
+            }
+            if (info[i].thumbHeight > height) {
+                height = info[i].thumbHeight;
+            }
+        }
+
+        return [width, height];
+    }
 }
-
 window.onload = desktop.init;
